7d4a74777c06995eb1f1a6b123194aaf8c461898
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2023-02-11 20:27:39 +0700

    Update auth.js

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,24 @@
+const Users = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const verifyToken = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        jwt.verify(token, process.env.SECRET, async (err, decoded) => {
+            if (err)
+                return res.status(403).json({message : "Unauthorized"})
+            const user = await Users.findById(decoded.id)
+            if(user){
+                req.user = user
+                next()
+            }
+            else{
+                res.status(403).json({message : "Unauthorized"})
+            }
+        })
+    } catch (error) {
+        res.status(403).json({message : error.message})
+    }
+}
+
+module.exports = verifyToken
