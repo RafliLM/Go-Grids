6be3bec7deef169d1e653232dd7bf56099553280
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2023-02-12 15:13:24 +0700

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router()
 router.use('/user', require('./user'))
 router.use('/journal', require('./journal'))
 router.use('/event', require('./event'))
+router.post('/create', API.createJournal)
 
 module.exports = router
