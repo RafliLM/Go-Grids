fe3f8aebaf29278b73543bf83e441a77830266c4
RijalAzmi <rijal.azmi.tif420@polban.ac.id>
2023-02-14 22:25:36 +0700

    Del Logo Github

--- FrontEnd/javascript-version/src/views/dashboards/analytics/AnalyticsAward.vue
+++ FrontEnd/javascript-version/src/views/dashboards/analytics/AnalyticsAward.vue
@@ -110,7 +110,7 @@ const triangleBg = computed(() => {
         </VCol>
       </VRow>
         <h1>Quotes</h1>
-        <VCard>
+        <VCard style="background-color: transparent;">
           <VCardText style="box-shadow: 0 0.5rem 0.5rem hsl(0 0% 0% / 10%); padding: 1rem; border-radius: 1rem">
             <p style="font-style: italic">
               “Some wishes are only there to teach us how to wait.”
