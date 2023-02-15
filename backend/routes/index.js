67939f191b96fb3b6e6990e8e4b85f727271b2f6
RafliLM <raflilutfiansyah28@gmail.com>
2023-02-15 20:44:29 +0700

    Implement update profile and reset password

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/user')
 
 router.post('/register', userController.register)
 router.post('/login', userController.login)
+router.post('/reset-password', userController.resetPassword)
 router.use('/user', verifyToken, require('./user'))
 router.use('/journal', verifyToken, require('./journal'))
 router.use('/event', verifyToken, require('./event'))
