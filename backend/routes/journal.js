3be095771d73f8a8fd7f5af97ad1d36a9ded7145
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2023-02-16 07:55:52 +0700

    change journal routes

--- backend/routes/journal.js
+++ backend/routes/journal.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const router = express.Router();
-const API = require('../controllers/journal');
+const router = express.Router()
+const API = require('../controllers/journal')
 
-
+router.post('/create', API.createJournal)
+router.get('/all', API.getAllJournals)
+router.get('/date/:date', API.getJournalsByDate)
+router.delete('/delete/:id', API.deleteJournals)
+router.patch('/update/:id', API.updateJournal)
 
 
 
