6eafdb0cd34d48fd9146b0da14770074e2dd5d33
RafliLM <raflilutfiansyah28@gmail.com>
2023-02-22 20:52:32 +0700

    Fix journal schema and some controller, and some event controller

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const API = require('../controllers/event');
 
-router.get('/', API.getEvent)
-router.post('/create', API.createEvent)
+router.get('/', API.getAllEvents)
+router.post('/', API.createEvent)
 router.patch('/:id', API.updateEvent)
 router.delete('/:id', API.deleteEvent)
 
