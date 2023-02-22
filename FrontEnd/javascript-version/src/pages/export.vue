4e9e00d1ee5e10882d525b90635aede07faebfea
fazamuhammadh <82070631+fazamuhammadh@users.noreply.github.com>
2023-02-23 01:07:15 +0700

    update generate button in export

--- FrontEnd/javascript-version/src/pages/export.vue
+++ FrontEnd/javascript-version/src/pages/export.vue
@@ -10,8 +10,12 @@ import TableFixedHeader from '@/views/user-interface/tables/TableFixedHeader.vue
   <VRow>
     <!-- basic -->
     <VCol cols="12">
-      <VCard>
-        <TableDark />
+      <VCard style="padding-left: 28px;">
+    <button style="background-color: #14162E; border-radius: 8px; height: 40px; width: 150px; ;">
+        <a style="color: white;">
+            generate to PDF
+        </a>
+    </button>
       </VCard>
     </VCol>
   </VRow>
