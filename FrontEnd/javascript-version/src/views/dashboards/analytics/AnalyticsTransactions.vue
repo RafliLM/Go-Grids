7d55ea4ab4e81ceb68db471d6f45129656e7bb3e
RijalAzmi <rijal.azmi.tif420@polban.ac.id>
2023-03-01 21:34:49 +0700

    Nambah Gambar di dashboard

--- FrontEnd/javascript-version/src/views/dashboards/analytics/AnalyticsTransactions.vue
+++ FrontEnd/javascript-version/src/views/dashboards/analytics/AnalyticsTransactions.vue
@@ -35,89 +35,91 @@ export default {
   data() {
     return {
       profile: {
-        username: localStorage.getItem("username"),
+        username: localStorage.getItem('username'),
       },
-      grid:[]
-    };
+      grid: [],
+    }
   },
   methods: {
     getProfile() {
-      const token = localStorage.getItem("token"); // membaca token dari local storage
+      const token = localStorage.getItem('token') // membaca token dari local storage
       const config = {
-        headers: { Authorization: `Bearer ${token}` } // mengirim token pada header permintaan
-      };
-      axios.get("//localhost:5000/api/user", config)
+        headers: { Authorization: `Bearer ${token}` }, // mengirim token pada header permintaan
+      }
+      axios
+        .get('//localhost:5000/api/user', config)
         .then(response => {
-          this.profile = response.data.firstname;
+          this.profile = response.data.firstname
         })
         .catch(error => {
-          console.log(error);
-        });
+          console.log(error)
+        })
     },
     showSwal() {
       Swal.fire({
-        title: "Add Journal ✏️",
+        title: 'Add Journal ✏️',
         width: '800px',
         html:
-        '<input id="swal-input1" class="swal2-input" autofocus placeholder="Journal Title" style="width:600px;">' +
-        '<textarea id="swal-input2" class="swal2-input" autogrow placeholder="Journal Content" style="width:600px; height:100px;">',
+          '<input id="swal-input1" class="swal2-input" autofocus placeholder="Journal Title" style="width:600px;">' +
+          '<textarea id="swal-input2" class="swal2-input" autogrow placeholder="Journal Content" style="width:600px; height:100px;">',
         confirmButtonText: 'Confirm',
         confirmButtonColor: '#14162E',
         cancelButtonText: 'Cancel',
         showCancelButton: true,
-      }).then((result) => {
-    if (result.isConfirmed) {
-      const question = document.getElementById('swal-input1').value;
-      const answer = document.getElementById('swal-input2').value;
-      const newGrid = { question, answer };
-      this.grid.push(newGrid);
-      const token = localStorage.getItem('token');
-      const config = {
-        headers: { Authorization: `Bearer ${token}` }
-      };
-      axios.post('//localhost:5000/api/journal/create', { question, answer }, config)
-        .then(response => {
-          console.log(response.data);
-          Swal.fire({
-            title: 'Success!',
-            text: 'Journal added successfully!',
-            icon: 'success',
-            confirmButtonText: 'OK'
-          });
-        })
-        .catch(error => {
-          console.log(error);
-          Swal.fire({
-            title: 'Error!',
-            text: 'Failed to add journal!',
-            icon: 'error',
-            confirmButtonText: 'OK'
-          });
-        });
-    }
-  });
-    }
+      }).then(result => {
+        if (result.isConfirmed) {
+          const question = document.getElementById('swal-input1').value
+          const answer = document.getElementById('swal-input2').value
+          const newGrid = { question, answer }
+          this.grid.push(newGrid)
+          const token = localStorage.getItem('token')
+          const config = {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+          axios
+            .post('//localhost:5000/api/journal/create', { question, answer }, config)
+            .then(response => {
+              console.log(response.data)
+              Swal.fire({
+                title: 'Success!',
+                text: 'Journal added successfully!',
+                icon: 'success',
+                confirmButtonText: 'OK',
+              })
+            })
+            .catch(error => {
+              console.log(error)
+              Swal.fire({
+                title: 'Error!',
+                text: 'Failed to add journal!',
+                icon: 'error',
+                confirmButtonText: 'OK',
+              })
+            })
+        }
+      })
+    },
   },
   mounted() {
-    this.getProfile();
-  }
-};
+    this.getProfile()
+  },
+}
 </script>
 
 
 <style type="text/css">
-.jarak{
+.jarak {
   margin-top: -20px;
 }
 .satu {
   font-size: 70px;
 }
-.gridTitle{
+.gridTitle {
   font-weight: bold;
   font-size: medium;
   color: black;
 }
-.gridContent{
+.gridContent {
   color: black;
   font-size: small;
 }
@@ -127,23 +129,39 @@ export default {
   <!-- Hallo Kurkur -->
   <VCard>
     <VCardItem>
-      <div class="pt-5 main" style="width: 200%">
-        <h1 class="pl-5">
-          Hello, {{ this.profile }} 👋
-        </h1>
+      <div
+        class="pt-5 main"
+        style="width: 200%"
+      >
+        <h1 class="pl-5">Hello, {{ this.profile }} 👋</h1>
         <p class="pl-5">How do you feel today?</p>
-        <a href="\" class="satu">
-        😀😭😡
+        <a
+          href="\"
+          class="satu"
+        >
+          😀😭😡
         </a>
 
         <h2 class="pl-5">Today's Journal</h2>
       </div>
-      <VCol cols="8" md="6">
+      <VCol
+        cols="8"
+        md="6"
+      >
         <div class="right">
-          <v-row justify="center" style="right: 200px; position: absolute">
-            <v-btn class="button-AddGrid" color="primary" @click="showSwal()"> + Add Grid Journal </v-btn>
+          <v-row
+            justify="center"
+            style="right: 200px; position: absolute"
+          >
+            <v-btn
+              class="button-AddGrid"
+              color="primary"
+              @click="showSwal()"
+            >
+              + Add Grid Journal
+            </v-btn>
 
-              <!-- <v-card height="44rem">
+            <!-- <v-card height="44rem">
                 <v-card-title class="pl-10">
                   <h1>Add Journal ✏️</h1>
                 </v-card-title>
@@ -187,50 +205,90 @@ export default {
                 </v-card-text>
               </v-card> -->
           </v-row>
-          <v-form ref="form" enctype="multipart/form-data" style="right: 48px; position: absolute; margin-top: -12px">
-            <v-btn 
-            outlined 
-            plain 
-            raised 
-            depressed 
-            elevation="2" 
-            type="submit" 
-            variant="#ffffff" 
-            color="black"
+          <v-form
+            ref="form"
+            enctype="multipart/form-data"
+            style="right: 48px; position: absolute; margin-top: -12px"
+          >
+            <v-btn
+              outlined
+              plain
+              raised
+              depressed
+              elevation="2"
+              type="submit"
+              variant="#ffffff"
+              color="black"
             >
               <strong>DELETE ALL</strong>
             </v-btn>
           </v-form>
         </div>
       </VCol>
-      
-      <div style="margin-top: 20px;"> 
+
+      <div style="margin-top: 20px">
         <VCol
           cols="10"
           sm="5"
           md="4"
         >
-
-          <v-col class="text-right" style="margin-bottom: -40px; margin-left: 20px; position: relative; z-index: 1;">
+          <v-col
+            class="text-right"
+            style="margin-bottom: -40px; margin-left: 20px; position: relative; z-index: 1"
+          >
             <v-spacer></v-spacer>
-            <v-btn size="x-small" right icon>
+            <v-btn
+              size="x-small"
+              right
+              icon
+            >
               <v-icon>mdi-close</v-icon>
             </v-btn>
           </v-col>
 
-          <VCard @click.stop="showSwalEdit()" style=" position: relative; z-index: 0; box-shadow: 0 0 0.5rem 0.5rem hsl(0 0% 0% / 10%); padding: 1rem; border-radius: 1rem;">            
+          <VCard
+            @click.stop="showSwalEdit()"
+            style="
+              position: relative;
+              z-index: 0;
+              box-shadow: 0 0 0.5rem 0.5rem hsl(0 0% 0% / 10%);
+              padding: 1rem;
+              border-radius: 1rem;
+            "
+          >
             <VCardItem>
               <VCardTitle class="gridTitle">Today's Goal</VCardTitle>
             </VCardItem>
 
-            <VCardText style="padding-bottom: 10px;" class="gridContent">
+            <VCardText
+              style="padding-bottom: 10px"
+              class="gridContent"
+            >
               Hari ini saya akan mengerjakan tugas mata kuliah Akuntansi
             </VCardText>
           </VCard>
         </VCol>
-
+        <div>
+          <center>
+            <VCard
+                  class="align-center justify-center auth-card"
+                  style="background-color: transparent; opacity: 50%"
+            >
+              <img
+                margin="10"
+                height="170"
+                width="200"
+                src="gglogo.png"
+            />
+            <VCol
+              class="text-b text-base"
+            >
+            <h3>You haven't add any journal</h3>
+            </VCol>
+            </VCard>
+          </center>
       </div>
-
+      </div>
     </VCardItem>
   </VCard>
 </template>
