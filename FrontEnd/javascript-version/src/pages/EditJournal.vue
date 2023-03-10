343e0de885b66b5fe23e35a39663eb8aad926775
RijalAzmi <rijal.azmi.tif420@polban.ac.id>
2023-03-10 20:02:44 +0700

    Page Edit Journal

---
+++ FrontEnd/javascript-version/src/pages/EditJournal.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import { useTheme } from 'vuetify'
+import moment from 'moment'
+const vuetifyTheme = useTheme()
+</script>
+
+<template>
+  <VCard>
+    <div>
+      <div class="grid-container right" style="padding-top: 30px; posis">
+          <h4
+            style="padding-left: 25px"
+            class="text-h4 font-weight-bold"
+          >
+            Edit Journal  ✏️
+          </h4>
+        <v-container fluid>
+          <h4> Title </h4>
+          <v-text-field
+            label="Title"
+            required
+          ></v-text-field>
+          <br>
+          <h4> Content </h4>
+          <QuillEditor
+            class="my-1"
+            theme="snow"
+            toolbar="essential"
+            style="height: 250px"
+          />
+        </v-container>
+        <v-form>
+          <v-btn
+            ref="form"
+            enctype="multipart/form-data"
+            style="right: 172px; position: absolute"
+            class="button-AddGrid"
+            depressed
+            elevation="2"
+            outlined
+            plain
+            raised
+            color="primary"
+            type="submit"
+            >submit
+          </v-btn>
+          <v-btn
+            style="right: 50px; position: absolute"
+            to="dashboard"
+            class="button-AddGrid"
+            color="on-primary"
+            variant="white"
+            @click="dashboard"
+            elevation="2"
+          >
+            <strong>Cancel</strong>
+          </v-btn>
+        </v-form>
+        <br><br>
+      </div>
+    </div>
+  </VCard>
+</template>
