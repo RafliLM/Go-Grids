40f899b2236c5c7a5462a271afc87ef3eb8d93f5
Gefi Sanjaya <76405521+gefisanjaya@users.noreply.github.com>
2023-04-03 11:09:17 +0700

    updateing update grid

--- FrontEnd/javascript-version/src/views/dashboards/analytics/Grid.vue
+++ FrontEnd/javascript-version/src/views/dashboards/analytics/Grid.vue
@@ -110,10 +110,9 @@ export default {
         })
     },
     showSwalEdit(journal, index) {
-      const journalId = journal._id
       const question = journal.grid[index].question
       const answer = journal.grid[index].answer
-      console.log(`<textarea id="swal-input2" class="swal2-input ans" value="${answer}" style="">`)
+
       Swal.fire({
         title: 'Edit Journal',
         html:
@@ -135,20 +134,17 @@ export default {
 
           return { newQuestion, newAnswer }
         },
-          }).then(result => {
-            if (result.isConfirmed) {
-              const token = localStorage.getItem('token');
-              const config = {
-                headers: { Authorization: `Bearer ${token}` },
-              }
-              const newData = {
-                grid: [
-                  {
-                    question: result.value.newQuestion,
-                    answer: result.value.newAnswer,
-                  },
-                ],
-              }
+      }).then(result => {
+        if (result.isConfirmed) {
+          const newData = {
+            grid: [
+              {
+                question: result.value.newQuestion,
+                answer: result.value.newAnswer,
+              },
+            ],
+          }
+          journal.grid[index] = newData.grid[0]
 
               axios.patch(`//localhost:5000/api/journal/${journalId}`, newData, config)
                 .then(response => {
